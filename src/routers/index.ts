0211158b3df7e394f580fe9dc100fd3adc396a00
Nguyen Phuong Nha <103343358+nphuonha2101@users.noreply.github.com>
2024-07-05 19:50:09 +0700

    feat: add ttsController to call Google Cloud API and return base64 audio to client

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,8 +1,14 @@
 import express, { Router } from "express";
 import rssController from "../controllers/rssController";
+import ttsController from "../controllers/ttsController";
+import multer from "multer";
 
 const rssRouter: Router = express.Router();
+const ttsRouter: Router = express.Router();
+
+const upload = multer();
 
 rssRouter.get("/rss/*", rssController.getRss);
+ttsRouter.post("/tts", upload.any(), ttsController.getTts);
 
-export { rssRouter };
+export { rssRouter, ttsRouter };
